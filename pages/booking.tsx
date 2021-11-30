e0a67922a1ed1501bfcb357cb1b57343e23adf8a
thanhvu2901 <vcthanh2901@gmail.com>
2021-11-30 17:23:23 +0700

    booking

---
+++ pages/booking.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import { Header, Main, Footer, Cards } from "@components";
+import { Booking } from "../src/components/booking/booking";
+
+const Book: React.FC = () => {
+    return <Booking />;
+};
+
+export default Book;
